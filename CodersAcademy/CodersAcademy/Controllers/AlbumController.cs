298a128ddae622f3a53b0a30f79dd4488f5eb4ac
Vinicius Pinho <vinipinho@gmail.com>
2020-11-18 15:35:07 -0300

    Primeira API Criada!!

---
+++ CodersAcademy/CodersAcademy/Controllers/AlbumController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAlbuns()
+        {
+            return Ok(new
+            {
+                Message = "Primeira API Criada"
+            });
+        }
+    }
+}
